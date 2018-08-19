96431ae233fe4b1bf00b46db147384dd1f3c4556
Christophe Garcia <christophegarcia01@gmail.com>
2018-08-19 13:55:39 +0200

    App in ES6

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const keySecret = "sk_test_CXosaxEOHMENrsmMbwBvGU5Q";
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const stripe = require("stripe")(keySecret);
 
 
 //var dataCardBike = [];
-var loginValid = "john@gmail.com";
-var passwordValid = "azerty";
+let loginValid = "john@gmail.com";
+let passwordValid = "azerty";
 
-var dataBike = [
+let dataBike = [
                   {name: "Model BIKO45", url:"/images/bike-1.jpg", price: 679},
                   {name: "Model ZOOK7", url:"/images/bike-2.jpg", price: 799},
                   {name: "Model LIKO89", url:"/images/bike-3.jpg", price: 839},
@@ -17,7 +17,7 @@ var dataBike = [
                   {name: "Model AMIG39", url:"/images/bike-6.jpg", price: 599}
                ];
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
 
   //console.log(req.session.dataCardBike);
   if(req.session.dataCardBike == undefined) {
@@ -30,25 +30,25 @@ router.get('/', function(req, res, next) {
   res.render('index', {dataBike: dataBike, isLogin: req.session.isLogin});
 });
 
-router.post('/add-card', function(req, res, next) {
+router.post('/add-card', (req, res, next) => {
   console.log(req.body);
   req.session.dataCardBike.push(req.body);
   res.render('card', { dataCardBike: req.session.dataCardBike});
 })
 
-router.post('/update-card', function(req, res, next) {
+router.post('/update-card', (req, res, next) => {
   console.log(req.body);
   req.session.dataCardBike[req.body.position].quantity = req.body.quantity;
   res.render('card', { dataCardBike: req.session.dataCardBike  });
 })
 
-router.get('/delete-card', function(req, res, next) {
+router.get('/delete-card', (req, res, next) => {
   console.log(req.body);
   req.session.dataCardBike.splice(req.query.position, 1);
   res.render('card', { dataCardBike : req.session.dataCardBike });
 })
 
-router.get('/card', function(req, res, next) {
+router.get('/card', (req, res, next) => {
 
   /*var totalCmd = 0;
   for(var i=0; i<dataCardBike.length; i++) {
@@ -58,14 +58,14 @@ router.get('/card', function(req, res, next) {
   res.render('card', { dataCardBike: req.session.dataCardBike });
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', (req, res, next) => {
   res.render('login-form', {msg: null});
 })
 
-router.post('/login', function(req, res, next) {
+router.post('/login', (req, res, next) => {
   console.log(req.body.email);
   console.log(req.body.password);
-  var isValid;
+  let isValid;
   if(req.body.email ==  loginValid && req.body.password == passwordValid) {
     req.session.isLogin = true;
     res.render('index', {dataBike: dataBike, isLogin: req.session.isLogin});
@@ -76,7 +76,7 @@ router.post('/login', function(req, res, next) {
   }
 })
 
-router.get('/logout', function(req, res, next) {
+router.get('/logout', (req, res, next) => {
   req.session.isLogin = false;
   req.session.dataCardBike = [];
   res.render('index', {dataBike: dataBike, isLogin: req.session.isLogin});
@@ -85,7 +85,7 @@ router.get('/logout', function(req, res, next) {
 router.post("/checkout", (req, res) => {
 
   var totalCmd = 0;
-  for(var i=0; i<req.session.dataCardBike.length; i++) {
+  for(let i=0; i<req.session.dataCardBike.length; i++) {
     totalCmd = totalCmd + (req.session.dataCardBike[i].price * req.session.dataCardBike[i].quantity);
   }
   totalCmd = totalCmd * 100;
